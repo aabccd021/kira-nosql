26b103f66ce38459da4adabb6f9edd6e732164ab
aabccd021 <aabccd021@gmail.com>
2021-05-14 18:49:19 +0700

    limit created collection action

--- test/on-create.test.ts
+++ test/on-create.test.ts
@@ -9,47 +9,48 @@ describe('makeOnCreateTrigger', () => {
       userColName: 'user',
       field: { type: 'count', countedCol: 'bookmark', groupByRef: 'bookmarkedarticle' },
     });
-    const articleTrigger = onCreateTrigger?.['article'];
-    const bookmarkTrigger = onCreateTrigger?.['bookmark'];
+    const articleAction = onCreateTrigger?.['article'];
+    const bookmarkAction = onCreateTrigger?.['bookmark'];
+
+    it('only create action for article and bookmark', async () => {
+      const actionColNames = Object.keys(onCreateTrigger ?? {});
+      expect(actionColNames).toStrictEqual(['article', 'bookmark']);
+    });
 
     it('init count field to 0', async () => {
-      const articleTriggerGetDoc: GetDoc = jest.fn();
+      const articleActionGetDoc: GetDoc = jest.fn();
 
-      const articleTriggerResult = await articleTrigger?.({
-        getDoc: articleTriggerGetDoc,
+      const articleActionResult = await articleAction?.({
+        getDoc: articleActionGetDoc,
         snapshot: { id: 'article-0', data: {} },
       });
 
-      expect(articleTriggerGetDoc).not.toHaveBeenCalled();
-      expect(articleTriggerResult).toStrictEqual({
+      expect(articleActionGetDoc).not.toHaveBeenCalled();
+      expect(articleActionResult).toStrictEqual({
         tag: 'right',
         value: {
           article: {
-            'article-0': {
-              bookmarkCount: 0,
-            },
+            'article-0': { bookmarkCount: 0 },
           },
         },
       });
     });
 
     it('increase count field when a document added on counted collection', async () => {
-      const bookmarkTriggerGetDoc: GetDoc = jest.fn();
+      const bookmarkActionGetDoc: GetDoc = jest.fn();
 
-      const bookmarkTriggerResult = await bookmarkTrigger?.({
-        getDoc: bookmarkTriggerGetDoc,
+      const bookmarkActionResult = await bookmarkAction?.({
+        getDoc: bookmarkActionGetDoc,
         snapshot: {
           id: 'bookmark-0',
           data: {
-            bookmarkedarticle: {
-              id: 'article-0',
-            },
+            bookmarkedarticle: { id: 'article-0' },
           },
         },
       });
 
-      expect(bookmarkTriggerGetDoc).not.toHaveBeenCalled();
-      expect(bookmarkTriggerResult).toStrictEqual({
+      expect(bookmarkActionGetDoc).not.toHaveBeenCalled();
+      expect(bookmarkActionResult).toStrictEqual({
         tag: 'right',
         value: {
           article: {
@@ -62,20 +63,18 @@ describe('makeOnCreateTrigger', () => {
     });
 
     it('returns error if id is not string', async () => {
-      const bookmarkTriggerGetDoc: GetDoc = jest.fn();
+      const bookmarkActionGetDoc: GetDoc = jest.fn();
 
-      const bookmarkTriggerResult = await bookmarkTrigger?.({
-        getDoc: bookmarkTriggerGetDoc,
+      const bookmarkActionResult = await bookmarkAction?.({
+        getDoc: bookmarkActionGetDoc,
         snapshot: {
           id: 'bookmark-0',
-          data: {
-            bookmarkedarticle: 0,
-          },
+          data: { bookmarkedarticle: 0 },
         },
       });
 
-      expect(bookmarkTriggerGetDoc).not.toHaveBeenCalled();
-      expect(bookmarkTriggerResult).toStrictEqual({
+      expect(bookmarkActionGetDoc).not.toHaveBeenCalled();
+      expect(bookmarkActionResult).toStrictEqual({
         tag: 'left',
         error: { errorType: 'invalid_data_type' },
       });
@@ -90,19 +89,16 @@ describe('makeOnCreateTrigger', () => {
         userColName: 'user',
         field: { type: 'creationTime' },
       });
-      const articleTrigger = onCreateTrigger?.['article'];
-      const articleTriggerGetDoc: GetDoc = jest.fn();
+      const articleAction = onCreateTrigger?.['article'];
+      const articleActionGetDoc: GetDoc = jest.fn();
 
-      const articleTriggerResult = await articleTrigger?.({
-        getDoc: articleTriggerGetDoc,
-        snapshot: {
-          id: 'article-0',
-          data: {},
-        },
+      const articleActionResult = await articleAction?.({
+        getDoc: articleActionGetDoc,
+        snapshot: { id: 'article-0', data: {} },
       });
 
-      expect(articleTriggerGetDoc).not.toHaveBeenCalled();
-      expect(articleTriggerResult).toStrictEqual({
+      expect(articleActionGetDoc).not.toHaveBeenCalled();
+      expect(articleActionResult).toStrictEqual({
         tag: 'right',
         value: {
           article: {
@@ -123,19 +119,7 @@ describe('makeOnCreateTrigger', () => {
         userColName: 'user',
         field: { type: 'image' },
       });
-      const articleTrigger = onCreateTrigger?.['article'];
-      const articleTriggerGetDoc: GetDoc = jest.fn();
-
-      const articleTriggerResult = await articleTrigger?.({
-        getDoc: articleTriggerGetDoc,
-        snapshot: {
-          id: 'article-0',
-          data: {},
-        },
-      });
-
-      expect(articleTriggerGetDoc).not.toHaveBeenCalled();
-      expect(articleTriggerResult).toBeUndefined();
+      expect(onCreateTrigger).toBeUndefined();
     });
   });
 });
