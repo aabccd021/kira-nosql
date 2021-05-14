544f62e9f38c0d526ebe6b4bb130783f4d423bb5
aabccd021 <aabccd021@gmail.com>
2021-05-14 11:21:57 +0700

    count test

--- test/on-create.test.ts
+++ test/on-create.test.ts
@@ -1,18 +1,69 @@
 import { makeOnCreateTrigger } from '../src/on-create';
+import { GetDoc } from '../src/types';
 
 describe('makeOnCreateTrigger', () => {
   describe('count field', () => {
-    it('returns right value', () => {
+    it('init count field to 0', async () => {
       const onCreateTrigger = makeOnCreateTrigger({
         colName: 'post',
         fieldName: 'bookmarkCount',
         userColName: 'user',
         field: { type: 'count', countedCol: 'bookmark', groupByRef: 'bookmarkedPost' },
       });
-      const postTrigger = onCreateTrigger['post']
-      const bookmarkTrigger = onCreateTrigger['bookmark']
 
-      const postTriggerResult = postTrigger({doc})
+      const postTrigger = onCreateTrigger?.['post'];
+      const postTriggerGetDoc: GetDoc = jest.fn();
+      const postTriggerResult = await postTrigger?.({
+        snapshot: { id: 'post-0', data: {} },
+        getDoc: postTriggerGetDoc,
+      });
+
+      expect(postTriggerGetDoc).not.toHaveBeenCalled();
+      expect(postTriggerResult).toStrictEqual({
+        tag: 'right',
+        value: {
+          post: {
+            'post-0': {
+              bookmarkCount: 0,
+            },
+          },
+        },
+      });
+    });
+
+    it('increase count field when a document added on counted collection', async () => {
+      const onCreateTrigger = makeOnCreateTrigger({
+        colName: 'post',
+        fieldName: 'bookmarkCount',
+        userColName: 'user',
+        field: { type: 'count', countedCol: 'bookmark', groupByRef: 'bookmarkedPost' },
+      });
+
+      const bookmarkTrigger = onCreateTrigger?.['bookmark'];
+      const bookmarkTriggerGetDoc: GetDoc = jest.fn();
+      const bookmarkTriggerResult = await bookmarkTrigger?.({
+        snapshot: {
+          id: 'bookmark-0',
+          data: {
+            bookmarkedPost: {
+              id: 'post-0',
+            },
+          },
+        },
+        getDoc: bookmarkTriggerGetDoc,
+      });
+
+      expect(bookmarkTriggerGetDoc).not.toHaveBeenCalled();
+      expect(bookmarkTriggerResult).toStrictEqual({
+        tag: 'right',
+        value: {
+          post: {
+            'post-0': {
+              bookmarkCount: { __fieldType: 'increment', value: 1 },
+            },
+          },
+        },
+      });
     });
   });
 });
