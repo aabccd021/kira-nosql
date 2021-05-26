aa644d6c3cb701b94cc8738616735c9821c97f6c
aabccd021 <aabccd021@gmail.com>
2021-05-26 15:43:30 +0700

    add test

--- test/on-create.test.ts
+++ test/on-create.test.ts
@@ -3,6 +3,8 @@ import {
   makeOnCreateCountFieldTrigger,
   makeOnCreateCreationTimeFieldTrigger,
   makeOnCreateImageFieldTrigger,
+  makeOnCreateOwnerFieldTrigger,
+  makeOnCreateRefFieldTrigger,
   makeOnCreateStringFieldTrigger,
 } from '../src/on-create';
 
@@ -138,6 +140,163 @@ describe('image field action maker', () => {
       field: { type: 'image' },
     });
     expect(onCreateTrigger).toBeUndefined();
+  });
+});
+
+describe('owner field action maker', () => {
+  const onCreateTrigger = makeOnCreateOwnerFieldTrigger({
+    colName: 'article',
+    fieldName: 'owner',
+    field: { type: 'owner' },
+    userColName: 'user',
+  });
+
+  const onCreateArticleTrigger = onCreateTrigger?.['article'];
+
+  it('only create action for article', () => {
+    const actionColNames = Object.keys(onCreateTrigger ?? {});
+    expect(actionColNames).toStrictEqual(['article']);
+  });
+
+  it('return error if refField is empty', async () => {
+    const articleActionGetDoc = jest.fn();
+    const articleActionResult = await onCreateArticleTrigger?.({
+      getDoc: articleActionGetDoc,
+      snapshot: { id: 'article0' },
+    });
+
+    expect(articleActionGetDoc).not.toHaveBeenCalled();
+    expect(articleActionResult).toStrictEqual({
+      tag: 'left',
+      error: { errorType: 'invalid_data_type' },
+    });
+  });
+
+  it('return error if refField is not type of ref field', async () => {
+    const articleActionGetDoc = jest.fn();
+    const articleActionResult = await onCreateArticleTrigger?.({
+      getDoc: articleActionGetDoc,
+      snapshot: {
+        id: 'article0',
+        data: {
+          owner: {
+            type: 'string',
+            value: 'somerandomstring',
+          },
+        },
+      },
+    });
+
+    expect(articleActionGetDoc).not.toHaveBeenCalled();
+    expect(articleActionResult).toStrictEqual({
+      tag: 'left',
+      error: { errorType: 'invalid_data_type' },
+    });
+  });
+
+  it('return error if get doc is error', async () => {
+    const mockedGetDocReturn: ReturnType<GetDoc<unknown>> = Promise.resolve({
+      tag: 'left',
+      error: 'error1',
+    });
+    const articleActionGetDoc = jest.fn().mockReturnValueOnce(mockedGetDocReturn);
+    const articleActionResult = await onCreateArticleTrigger?.({
+      getDoc: articleActionGetDoc,
+      snapshot: {
+        id: 'article0',
+        data: {
+          owner: {
+            type: 'ref',
+            value: { id: 'user0' },
+          },
+        },
+      },
+    });
+
+    expect(articleActionGetDoc).toHaveBeenCalledTimes(1);
+    expect(articleActionGetDoc).toHaveBeenCalledWith({ col: 'user', id: 'user0' });
+    expect(articleActionResult).toStrictEqual({
+      tag: 'left',
+      error: 'error1',
+    });
+  });
+});
+
+describe('ref field action maker', () => {
+  const onCreateTrigger = makeOnCreateRefFieldTrigger({
+    colName: 'articleReply',
+    fieldName: 'repliedArticle',
+    field: { type: 'ref', refCol: 'article' },
+  });
+
+  const onCreateArticleReplyTrigger = onCreateTrigger?.['articleReply'];
+
+  it('only create action for articleReply', () => {
+    const actionColNames = Object.keys(onCreateTrigger ?? {});
+    expect(actionColNames).toStrictEqual(['articleReply']);
+  });
+
+  it('return error if refField is empty', async () => {
+    const articleReplyActionGetDoc = jest.fn();
+    const articleReplyActionResult = await onCreateArticleReplyTrigger?.({
+      getDoc: articleReplyActionGetDoc,
+      snapshot: { id: 'articleReply0' },
+    });
+
+    expect(articleReplyActionGetDoc).not.toHaveBeenCalled();
+    expect(articleReplyActionResult).toStrictEqual({
+      tag: 'left',
+      error: { errorType: 'invalid_data_type' },
+    });
+  });
+
+  it('return error if refField is not type of ref field', async () => {
+    const articleReplyActionGetDoc = jest.fn();
+    const articleReplyActionResult = await onCreateArticleReplyTrigger?.({
+      getDoc: articleReplyActionGetDoc,
+      snapshot: {
+        id: 'articleReply0',
+        data: {
+          owner: {
+            type: 'string',
+            value: 'somerandomstring',
+          },
+        },
+      },
+    });
+
+    expect(articleReplyActionGetDoc).not.toHaveBeenCalled();
+    expect(articleReplyActionResult).toStrictEqual({
+      tag: 'left',
+      error: { errorType: 'invalid_data_type' },
+    });
+  });
+
+  it('return error if get doc is error', async () => {
+    const mockedGetDocReturn: ReturnType<GetDoc<unknown>> = Promise.resolve({
+      tag: 'left',
+      error: 'error1',
+    });
+    const articleReplyActionGetDoc = jest.fn().mockReturnValueOnce(mockedGetDocReturn);
+    const articleReplyActionResult = await onCreateArticleReplyTrigger?.({
+      getDoc: articleReplyActionGetDoc,
+      snapshot: {
+        id: 'articleReply0',
+        data: {
+          repliedArticle: {
+            type: 'ref',
+            value: { id: 'article0' },
+          },
+        },
+      },
+    });
+
+    expect(articleReplyActionGetDoc).toHaveBeenCalledTimes(1);
+    expect(articleReplyActionGetDoc).toHaveBeenCalledWith({ col: 'article', id: 'article0' });
+    expect(articleReplyActionResult).toStrictEqual({
+      tag: 'left',
+      error: 'error1',
+    });
   });
 });
 
