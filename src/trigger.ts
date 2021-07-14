7c2492ac5dc0e15e2f5dea42c77d013cc306a70e
aabccd021 <aabccd021@gmail.com>
2021-07-14 13:09:01 +0700

    transaction commit

--- src/trigger.ts
+++ src/trigger.ts
@@ -1,4 +1,3 @@
-import assertNever from 'assert-never';
 import { Dictionary, Field } from 'kira-core';
 
 import {
@@ -8,11 +7,12 @@ import {
   DataTypeError,
   DB,
   DocCommit,
-  DocKey,
   Draft,
   Either,
+  GetDoc,
   MakeDraft,
   SnapshotOfActionType,
+  TransactionCommit,
 } from './type';
 
 function isDefined<T>(t: T | undefined): t is T {
@@ -54,7 +54,69 @@ export function getDraft<F extends Field, GDE, WR>({
   );
 }
 
-export async function runTrigger<A extends ActionType, GDE, WR>({
+export async function getTransactionCommit<A extends ActionType, GDE, WR>({
+  draft,
+  snapshot,
+  getDoc,
+}: {
+  readonly draft: ColDrafts<A, GDE, WR>;
+  readonly snapshot: SnapshotOfActionType<A>;
+  readonly getDoc: GetDoc<GDE>;
+}): Promise<Either<TransactionCommit, DataTypeError | GDE>> {
+  return Promise.all(draft.getTransactionCommits.map((gtc) => gtc({ getDoc, snapshot }))).then(
+    (transactionCommits) =>
+      transactionCommits.reduce(
+        (prevTC, currentTC) => {
+          if (prevTC.tag === 'left') return prevTC;
+          if (currentTC.tag === 'left') return currentTC;
+          return {
+            tag: 'right',
+            value: {
+              // merge Transaction commit
+              ...currentTC.value,
+              ...Object.fromEntries(
+                Object.entries(prevTC.value).map(([colName, col]) => {
+                  const col2 = currentTC.value[colName] ?? {};
+                  return [
+                    colName,
+                    {
+                      ...col2,
+                      ...Object.fromEntries(
+                        // Merge doc operations. Operation priority is delete > merge.
+                        Object.entries(col).map<readonly [string, DocCommit]>(([docId, docOp]) => {
+                          const docOp2 = col2[docId];
+                          if (docOp.op === 'delete' || docOp2?.op === 'delete') {
+                            return [docId, { op: 'delete' }];
+                          }
+                          if (!docOp2) {
+                            return [docId, docOp];
+                          }
+                          /**
+                           * If both prev.value and e.value provide merge value for the same field,
+                           * value from e.value will be used
+                           */
+                          return [
+                            docId,
+                            {
+                              op: 'merge',
+                              data: { ...docOp.data, ...docOp2.data },
+                            },
+                          ];
+                        })
+                      ),
+                    },
+                  ];
+                })
+              ),
+            },
+          };
+        },
+        { tag: 'right', value: {} }
+      )
+  );
+}
+
+export async function runMayFailOps<A extends ActionType, GDE, WR>({
   draft,
   snapshot,
   db,
@@ -62,74 +124,8 @@ export async function runTrigger<A extends ActionType, GDE, WR>({
   readonly draft: ColDrafts<A, GDE, WR>;
   readonly snapshot: SnapshotOfActionType<A>;
   readonly db: DB<GDE, WR>;
-}): Promise<Either<readonly WR[], DataTypeError | GDE>> {
-  const transactionCommit = await Promise.all(
-    draft.getTransactionCommits.map((gtc) => gtc({ ...db, snapshot }))
-  ).then((transactionCommits) =>
-    transactionCommits.reduce(
-      (prevTC, currentTC) => {
-        if (prevTC.tag === 'left') return prevTC;
-        if (currentTC.tag === 'left') return currentTC;
-        return {
-          tag: 'right',
-          value: {
-            // merge Transaction commit
-            ...currentTC.value,
-            ...Object.fromEntries(
-              Object.entries(prevTC.value).map(([colName, col]) => {
-                const col2 = currentTC.value[colName] ?? {};
-                return [
-                  colName,
-                  {
-                    ...col2,
-                    ...Object.fromEntries(
-                      // Merge doc operations. Operation priority is delete > merge.
-                      Object.entries(col).map<readonly [string, DocCommit]>(([docId, docOp]) => {
-                        const docOp2 = col2[docId];
-                        if (docOp.op === 'delete' || docOp2?.op === 'delete') {
-                          return [docId, { op: 'delete' }];
-                        }
-                        if (!docOp2) {
-                          return [docId, docOp];
-                        }
-                        /**
-                         * If both prev.value and e.value provide merge value for the same field,
-                         * value from e.value will be used
-                         */
-                        return [
-                          docId,
-                          {
-                            op: 'merge',
-                            data: { ...docOp.data, ...docOp2.data },
-                          },
-                        ];
-                      })
-                    ),
-                  },
-                ];
-              })
-            ),
-          },
-        };
-      },
-      { tag: 'right', value: {} }
-    )
-  );
-  if (transactionCommit.tag === 'left') {
-    return transactionCommit;
-  }
-  const result = await Promise.all(
-    Object.entries(transactionCommit.value).flatMap(([colName, docs]) =>
-      Object.entries(docs).map(([docId, doc]) => {
-        const key: DocKey = { col: colName, id: docId };
-        if (doc.op === 'merge') return db.mergeDoc({ key, docData: doc.data });
-        if (doc.op === 'delete') return db.deleteDoc({ key });
-        assertNever(doc);
-      })
-    )
-  );
+}): Promise<void> {
   Promise.all(draft.mayFailOps.map((mayFailOp) => mayFailOp({ ...db, snapshot })));
-  return { tag: 'right', value: result };
 }
 
 export function isTriggerRequired<A extends ActionType, GDE, WR>(
