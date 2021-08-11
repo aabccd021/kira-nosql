a3ac0d535756ec83f0ae3faa7b95810eb2eeb9d4
aabccd021 <aabccd021@gmail.com>
2021-08-11 15:48:04 +0700

    buildDraft

--- src/field/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './count';
-export * from './creation-time';
-export * from './ref';
-// TODO: eslint-plugin-kira-client: dont import kira libary, use generated instead
-// TODO: eslint-plugin-kira-next: dont use more than one hooks in page
